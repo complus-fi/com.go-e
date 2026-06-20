6f3d5702daad8f18711f9f8c3d0633634937fbda
Tapio Heiskanen <tapio.heiskanen@iki.fi>
2026-06-20 07:49:40 +0300

    Add TypeScript configuration file (tsconfig.json) for project setup

--- drivers/evcharger-cloud-device.js
+++ drivers/evcharger-cloud-device.js
@@ -1,13 +1,8 @@
 'use strict';
 
-const evChargerDevice = require('evcharger-device');
+const evChargerDevice = require('./evcharger-device');
 const goeChargerAPI = require('../lib/go-eCharger-API-v2');
-const { GOE_CHARGER_MODE, getStatusAttributes, mapHomeyToApiValues, mapStatusToCapabilities } = require('../lib/mappings');
-
-const POLL_INTERVAL = 5000;
-const CHARGING_UI_DEBOUNCE_POLLS = 1;
-const AUTO_SPL3_THRESHOLD_W = 4140;
-const GOE_CHARGER_MODE_IDS = new Set(Object.values(GOE_CHARGER_MODE));
+const { getStatusAttributes } = require('../lib/mappings');
 
 class evCloudChargerDevice extends evChargerDevice.Device {
   getApiBaseUrl(serialnumber) {
@@ -25,6 +20,7 @@ class evCloudChargerDevice extends evChargerDevice.Device {
     const settings = this.getSettings();
     this.api = new goeChargerAPI();
     this.api.base_url = this.getApiBaseUrl(settings.serialnumber);
+    this.api.cloud_api_token = this.getApiBaseUrl(settings.token);
     this.api.driver = this.driver.id;
 
     await this.checkCapabilities();
